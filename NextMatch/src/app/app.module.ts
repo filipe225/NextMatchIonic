86be20dabb39ba19053b572025fc26f770dd604f
Filipe Sousa <hadespower@gmail.com>
2021-04-24 00:24:29 +0100

    firebase added

--- NextMatch/src/app/app.module.ts
+++ NextMatch/src/app/app.module.ts
@@ -7,11 +7,22 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { firebaseConfig } from '../environments/environment';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore/';
 
 @NgModule({
 	declarations: [AppComponent],
 	entryComponents: [],
-	imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+	imports: [
+		BrowserModule, 
+		IonicModule.forRoot(), 
+		AppRoutingModule,
+		AngularFireModule.initializeApp(firebaseConfig),
+		AngularFireAuthModule,
+		AngularFirestoreModule
+	],
 	providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
 	bootstrap: [AppComponent],
 })
